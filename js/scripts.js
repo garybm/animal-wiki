475cc605ef4d03fb13f4f6a154a2994c6c912cdd
Gary Bermudez and Tracy Jo Reith <tracy@>
2019-01-17 11:59:25 -0800

    add images, if and else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
-// $(document).ready(function() {
-//   var age = parseInt(prompt("How old are you?"));
-//
-//   if (age >= 18) {
-//     $('#vote').show();
-//   } else {
-//     $('#under-18').show();
-//   }
-// });
+$(document).ready(function() {
+
+  var animal = $("#animal").val();
+  if (animal === "animal1") {
+    $('#animal1').show();
+    $('#animal2').hide()
+    $('#animal3').hide()
+  } else if (animal === "animal2") {
+    $('#animal2').show();
+    $('#animal1').hide()
+    $('#animal3').hide()
+  } else if (animal === "animal3") {
+    $('#animal3').show();
+    $('#animal1').hide()
+    $('#animal2').hide()
+  } else {}
+}
+
+});
